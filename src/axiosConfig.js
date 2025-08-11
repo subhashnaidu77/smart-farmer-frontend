476ed77b433c74365ba4cfd55a9ada2b73389c22
subhashnaidu77 <subhashnaidu77@gmail.com>
2025-08-11 19:51:29 +0530

    deploy3

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // --- PASTE YOUR LIVE RENDER URL HERE ---
 // This is the URL you copied from the Render dashboard
 // const API_BASE_URL = 'https://smart-farmer-backend.onrender.com'; 
-const API_BASE_URL = 'https://smart-farmer-backend-1.onrender.com'; 
+// const API_BASE_URL = 'https://smart-farmer-backend-1.onrender.com'; 
+const API_BASE_URL='https://smart-farmer-backend-2.onrender.com';
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
 });
